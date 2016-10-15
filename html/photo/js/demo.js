c31b343f2bd288f8121851e085c3d2d7947d54da
xuqi1987 <xuqi.jx@gmail.com>
2016-10-15 23:15:30 +0800

    fix path

--- html/photo/js/demo.js
+++ html/photo/js/demo.js
@@ -25,7 +25,7 @@ $(function () {
         $.each(data, function(indx, item){
 
           carouselLinks.push({
-            href: './photo/' + item[0] + '.jpg',
+            href: '../' + item[0] + '.jpg',
             title: ''
           })          
         });
