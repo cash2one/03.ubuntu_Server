36dba5efef6ccf4dfee4b48a56746b6cc5b579ca
xuqi_1 <xuqi.jx@gmail.com>
2016-11-03 20:57:32 +0800

    add test aria2

--- aria2/test.py
+++ aria2/test.py
@@ -1,3 +1,13 @@
-from pyaria2 import PyAria2
+# -*- coding: utf-8 -*-
 
+from pyaria2 import PyAria2
+import time
 a = PyAria2()
+gid = a.addUri([u'ftp://a:a@dygod18.com:21/[电影天堂www.dy2018.com]名侦探柯南：纯黑的噩梦BD日语中字.mkv'])
+
+print gid
+
+while True:
+	result =  a.tellStatus(gid)
+	print "speed:%(downloadSpeed)s \tstate:%(status)s\t %(completedLength)s | %(totalLength)s" % result
+	time.sleep(1)
