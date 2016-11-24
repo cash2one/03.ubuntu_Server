82aa0581fc83cd391957160f5fab62406aaeffe7
xuqi_1 <xuqi.jx@gmail.com>
2016-11-24 19:52:00 +0800

    movify the resetful

--- resetful/resetfulutils.py
+++ resetful/resetfulutils.py
@@ -1,25 +1,28 @@
 # -*- coding:utf8 -*-
 from aria2.pyaria2 import *
-from database.movieDB import MovieModule,before_request_handler,after_request_handler,movieinfo as Movie_tb,links as Links_tb
+from database.datamodule import before_request_handler,after_request_handler,tb_movies,tb_links
 import urllib
 
 aria2 = PyAria2()
 print aria2.getGlobalOption()
 
 def download(id,path='/data/download'):
-    data = [dic for dic in  Links_tb.select().where(Links_tb.id == id).dicts()]
+    data = [dic for dic in  tb_links.select().where(tb_links.id == id).dicts()]
     print data
     result = []
     if len(data) > 0:
         gid = data[0]['gid']
 
-        if gid == None:
+        if len(gid) == 0:
             print urllib.unquote(data[0]['sourceurl'].encode('UTF-8'))
             gid = aria2.addUri([urllib.unquote(data[0]['sourceurl'].encode('UTF-8'))],{"dir":path})
             result = aria2.tellStatus(gid)
         else:
             result = aria2.tellStatus(gid)
-        query = Links_tb.update(gid = gid,status=result['status'],downloadpath=result['files'][0]['path']).where(Links_tb.id == id)
+        print "-"*100
+        print result
+        print "-"*100
+        query = tb_links.update(gid = gid,status=result['status'],downloadpath=result['files'][0]['path']).where(tb_links.id == id)
         query.execute()
 
         print result['status']
