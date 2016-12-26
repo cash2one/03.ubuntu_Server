f93941ef1db723b15750fce4cd14894758838a1f
xuqi1987 <xuqi.jx@gmail.com>
2016-12-27 07:25:52 +0800

    modify image

--- html/movie/js/controllers/MovieListController.js
+++ html/movie/js/controllers/MovieListController.js
@@ -50,5 +50,62 @@ app.controller('MovieListController', function($scope, $http,$routeParams,movied
         });
     };
 
+    $scope.loadimage = function(index,url) {
+
+        //loading参数
+        var opts = {
+            lines: 12 // The number of lines to draw
+            , length: 6 // The length of each line
+            , width: 6 // The line thickness
+            , radius: 10 // The radius of the inner circle
+            , scale: 1 // Scales overall size of the spinner
+            , corners: 1 // Corner roundness (0..1)
+            , color: '#333' // #rgb or #rrggbb or array of colors
+            , opacity: 0.25 // Opacity of the lines
+            , rotate: 0 // The rotation offset
+            , direction: 1 // 1: clockwise, -1: counterclockwise
+            , speed: 1 // Rounds per second
+            , trail: 48 // Afterglow percentage
+            , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+            , zIndex: 99999 // The z-index (defaults to 2000000000)
+            , className: 'spinner' // The CSS class to assign to the spinner
+            , top: '50%' // Top position relative to parent
+            , right: '50%' // Left position relative to parent
+            , shadow: false // Whether to render a shadow
+            , hwaccel: false // Whether to use hardware acceleration
+            , position: 'absolute' // Element positioning
+        };
+
+        var target = $("#image"+index)
+
+        var spinner = new Spinner(opts).spin(target);
+
+        var surl=url + '?x-oss-process=image/resize,m_fill,h_370,w_250';
+
+        function imgLoadAsync(url){
+            return new Promise(function(resolve,reject){
+                var img=new Image();
+                img.onload=function(){
+                    resolve(img);
+                }
+                img.onerror=function(){
+                    img.src = "http://x2020-movie.oss-cn-shanghai.aliyuncs.com/NotFind.jpg?x-oss-process=image/resize,m_pad,h_370,w_250,color_ffffff"
+                    reject(img);
+                }
+                img.src=url;
+
+
+            });
+        }
+
+        imgLoadAsync(surl).then(function(data){
+            console.log("success")
+            spinner.stop();
+            $("#image"+index).append(data);
+        },function(data) {
+            $("#image"+index).append(data);
+        })
+    };
+
 
 });
