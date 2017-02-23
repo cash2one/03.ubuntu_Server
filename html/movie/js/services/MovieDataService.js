7454a571bf197d1b67769a30f1e4067a7e2b7501
xuqi_1 <xuqi.jx@gmail.com>
2017-02-23 11:48:42 +0800

    Release 0.1

--- html/movie/js/services/MovieDataService.js
+++ html/movie/js/services/MovieDataService.js
@@ -3,8 +3,8 @@
  */
 app.factory('moviedataservice',['$http','$q','$resource',function ($http,$q,$resource) {
 
-    //root = "http://x2020.top/v1"
-    root = "http://localhost:5002"
+    root = "http://x2020.top/v1"
+    //root = "http://localhost:5002"
 
 
     req_get = function(path) {
